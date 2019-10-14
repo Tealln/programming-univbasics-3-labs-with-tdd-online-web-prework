864a236063f7d63a954d0c028b0bcca3bf227e82
Tempesst Allen <elisetempesst@gmail.com>
2019-10-14 18:16:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 
 first_number = get_variable_from_file 
 expect(first_number).to be_an(Integer).or be_a(Float)
-  
+expect(second_number).to be an(Integernteger).or be_a(Float)
 expect(second_number) . not_to equal(0)
